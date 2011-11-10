0c4f1e899eec353f17d987859d641cb9dca58072
Isadenghien <Isadenghien@users.noreply.github.com>
2011-11-10 16:25:03 +0000

    Change the path to launch runFreesurferCommand.sh from scripts directory
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@45523 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/freesurfer/brainvisaFreesurfer.py
+++ python/freesurfer/brainvisaFreesurfer.py
@@ -2,17 +2,43 @@
 import os
 from neuroConfig import mainPath
 
-runFreesurferCommandSh = os.path.join( mainPath, 'toolboxes', 'freesurfer', 'scripts', 'runFreesurferCommand.sh' )
+print 'PATH'
+print mainPath
+
+#runFreesurferCommandSh = os.path.join( mainPath, '..', 'scripts', 'runFreesurferCommand.sh' )
+#path_freesurfer = "/volatile/svn/source/cortical_surface/freesurfer_plugin/trunk/brainvisa/scripts/runFreesurferCommand.sh"
+
+path_basename = os.path.dirname(mainPath)
+print 'Dirname'
+print path_basename
+
+#runFreesurferCommandSh = os.path.join('/', 'volatile', 'svn', 'source', 'cortical_surface', 'freesurfer_plugin', 'trunk', 'scripts', 'runFreesurferCommand.sh' )
+#runFreesurferCommandSh = os.path.join('/', 'volatile', 'svn', 'compil', 'scripts', 'runFreesurferCommand.sh' )
+runFreesurferCommandSh = os.path.join(path_basename, 'scripts', 'runFreesurferCommand.sh' )
+
+
+#runFreesurferCommandSh = '/volatile/svn/source/cortical_surface/freesurfer_plugin/trunk/brainvisa/scripts/runFreesurferCommand.sh'
+print "PATH FREESURFER"
+print runFreesurferCommandSh
+
+
+#runFreesurferCommandSh = os.path.join( mainPath, 'toolboxes', 'freesurfer', 'scripts', 'runFreesurferCommand.sh' )
 if not os.path.exists( runFreesurferCommandSh ):
+ print "path don't exist"
  runFreesurferCommandSh = None
  
 def launchFreesurferCommand( context, database, *args, **kwargs ):
   if database:
     os.environ[ 'SUBJECTS_DIR' ] = database
+  print "comamnd"
+  print runFreesurferCommandSh
   if runFreesurferCommandSh:
     print 'run'
     print args
     print kwargs
     context.system( *( ( runFreesurferCommandSh, ) + args ), **kwargs )
   else:
+    print 'else'
+    print args
+    print kwargs
     context.system( *args, **kwargs )
