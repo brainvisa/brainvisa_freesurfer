a010fb8ea8dc146c58f41f3c808c5bc9a2adcef6
Isadenghien <Isadenghien@users.noreply.github.com>
2012-01-16 14:49:21 +0000

    comment error messages
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@46401 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/freesurfer/brainvisaFreesurfer.py
+++ python/freesurfer/brainvisaFreesurfer.py
@@ -2,15 +2,15 @@
 import os
 from neuroConfig import mainPath
 
-print 'PATH'
-print mainPath
+#print 'PATH'
+#print mainPath
 
 #runFreesurferCommandSh = os.path.join( mainPath, '..', 'scripts', 'runFreesurferCommand.sh' )
 #path_freesurfer = "/volatile/svn/source/cortical_surface/freesurfer_plugin/trunk/brainvisa/scripts/runFreesurferCommand.sh"
 
 path_basename = os.path.dirname(mainPath)
-print 'Dirname'
-print path_basename
+#print 'Dirname'
+#print path_basename
 
 #runFreesurferCommandSh = os.path.join('/', 'volatile', 'svn', 'source', 'cortical_surface', 'freesurfer_plugin', 'trunk', 'scripts', 'runFreesurferCommand.sh' )
 #runFreesurferCommandSh = os.path.join('/', 'volatile', 'svn', 'compil', 'scripts', 'runFreesurferCommand.sh' )
@@ -18,27 +18,27 @@ runFreesurferCommandSh = os.path.join(path_basename, 'scripts', 'runFreesurferCo
 
 
 #runFreesurferCommandSh = '/volatile/svn/source/cortical_surface/freesurfer_plugin/trunk/brainvisa/scripts/runFreesurferCommand.sh'
-print "PATH FREESURFER"
-print runFreesurferCommandSh
+#print "PATH FREESURFER"
+#print runFreesurferCommandSh
 
 
 #runFreesurferCommandSh = os.path.join( mainPath, 'toolboxes', 'freesurfer', 'scripts', 'runFreesurferCommand.sh' )
 if not os.path.exists( runFreesurferCommandSh ):
- print "path don't exist"
+ #print "path don't exist"
  runFreesurferCommandSh = None
  
 def launchFreesurferCommand( context, database, *args, **kwargs ):
   if database:
     os.environ[ 'SUBJECTS_DIR' ] = database
-  print "comamnd"
-  print runFreesurferCommandSh
+  #print "comamnd"
+  #print runFreesurferCommandSh
   if runFreesurferCommandSh:
-    print 'run'
-    print args
-    print kwargs
+    #print 'run'
+    #print args
+    #print kwargs
     context.system( *( ( runFreesurferCommandSh, ) + args ), **kwargs )
   else:
-    print 'else'
-    print args
-    print kwargs
+    #print 'else'
+    #print args
+    #print kwargs
     context.system( *args, **kwargs )
