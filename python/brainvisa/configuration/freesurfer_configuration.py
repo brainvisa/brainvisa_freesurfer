c10f03a906e9ea9aa34507b0c2acf409dd619bef
dgeffroy <dgeffroy@users.noreply.github.com>
2012-06-27 12:21:38 +0000

    #3064 soma.wip.configuration -> soma.configuration
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@49735 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/brainvisa/configuration/freesurfer_configuration.py
+++ python/brainvisa/configuration/freesurfer_configuration.py
@@ -39,7 +39,7 @@ __docformat__ = "epytext en"
 
 import os
 import distutils.spawn
-from soma.wip.configuration import ConfigurationGroup
+from soma.configuration import ConfigurationGroup
 from soma.signature.api import Signature, Unicode, FileName, Sequence, Boolean
 
 
