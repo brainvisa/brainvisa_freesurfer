8b83f9d93782b0e976e871e391425a3346d7eaf7
lefranc <lefranc@bioproj.invalid>
2012-07-12 12:40:51 +0000

    allow FreeSurfer CreateSubject process to work outside a database, and make a link between the input image and the subject name.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@49956 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/freesurferCreateSubject.py
+++ brainvisa/toolboxes/freesurfer/processes/freesurferCreateSubject.py
@@ -2,28 +2,35 @@ import os
 from brainvisa.processes import *
 from freesurfer.brainvisaFreesurfer import launchFreesurferCommand
 
-name = "01 Create Freesurfer subject from Dicom anatomical image"
+name = "01 Create Freesurfer subject from T1 anatomical image"
 userLevel = 2
 
 signature = Signature(
-  'DicomImage', ReadDiskItem('Any Type', getAllFormats()),
+  'RawT1Image', ReadDiskItem('Raw T1 MRI', getAllFormats()),
   'subjectName', String(),
   'database', Choice(),
   )
 
 def initialization(self):
-  databases=[(h.name, h) for h in reversed(neuroHierarchy.hierarchies())]# reverse order of hierarchies to have brainvisa shared hierarchy at the end of the list
+  def linkSubjectName( proc, dummy ):
+    if proc.RawT1Image is not None:
+      return os.path.basename( proc.RawT1Image.fullName() )
+  databases=[h.name for h in reversed(neuroHierarchy.hierarchies())]# reverse order of hierarchies to have brainvisa shared hierarchy at the end of the list
   self.signature['database'].setChoices(*databases)
-  self.database=databases[0][1] 
+  if len( databases ) != 0:
+    self.database=databases[0]
+  else:
+    self.signature[ 'database' ] = OpenChoice()
+  self.linkParameters( 'subjectName', 'RawT1Image', linkSubjectName )
 
   
 def execution(self, context):
-  context.write('Create subject hierarchy and convert Dicom image to mgz format.')
-  cmd = 'recon-all -i %s -subjid %s'%(self.DicomImage.fullPath(), self.subjectName)
+  context.write('Create subject hierarchy and convert image to mgz format.')
+  cmd = 'recon-all -i %s -subjid %s'%(self.RawT1Image.fullPath(), self.subjectName)
   context.write(cmd)
-  context.write(self.database.name)
-  launchFreesurferCommand(context, self.database.name, 'recon-all', '-i', self.DicomImage.fullPath(), '-subjid', self.subjectName)
-  createdDir = self.database.name+'/'+self.subjectName
+  context.write(self.database)
+  launchFreesurferCommand(context, self.database, 'recon-all', '-i', self.RawT1Image.fullPath(), '-subjid', self.subjectName)
+  createdDir = self.database+'/'+self.subjectName
   context.write("Updating database, path = " + createdDir)
   neuroHierarchy.databases.update([createdDir])
   #neuroHierarchy.databases.update()
