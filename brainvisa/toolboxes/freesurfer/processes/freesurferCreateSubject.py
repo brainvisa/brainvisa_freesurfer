635f96c037931315e7fa589947f3e8ce27be596c
lefranc <lefranc@bioproj.invalid>
2012-07-17 08:27:45 +0000

    Add an additional output parameter named AnatImage, is completely hidden for the user.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@50014 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/freesurferCreateSubject.py
+++ brainvisa/toolboxes/freesurfer/processes/freesurferCreateSubject.py
@@ -9,31 +9,37 @@ signature = Signature(
   'RawT1Image', ReadDiskItem('Raw T1 MRI', getAllFormats()),
   'subjectName', String(),
   'database', Choice(),
+  'AnatImage', WriteDiskItem( 'FreesurferAnat', 'FreesurferMGZ' )
   )
 
 def initialization(self):
   def linkSubjectName( proc, dummy ):
     if proc.RawT1Image is not None:
-      return os.path.basename( proc.RawT1Image.fullName() )
-  databases=[h.name for h in reversed(neuroHierarchy.hierarchies())]# reverse order of hierarchies to have brainvisa shared hierarchy at the end of the list
+      return os.path.basename( os.path.dirname( os.path.dirname( os.path.dirname( proc.RawT1Image.fullName() ) ) ) )
+  databases=[h.name for h in neuroHierarchy.hierarchies() if h.fso.name == 'freesurfer']
   self.signature['database'].setChoices(*databases)
   if len( databases ) != 0:
     self.database=databases[0]
   else:
     self.signature[ 'database' ] = OpenChoice()
   self.linkParameters( 'subjectName', 'RawT1Image', linkSubjectName )
-
+  def linkAnatImage( proc, dummy ):
+    if proc.subjectName is not None and proc.database is not None:
+      subject = proc.subjectName
+      dirname = proc.database
+      print 'subject:', subject
+      print 'dirname:', dirname
+      filename = os.path.join( dirname, subject, 'mri/orig/001.mgz' )
+      return filename
+  self.linkParameters( 'AnatImage', ( 'subjectName', 'database' ), linkAnatImage )
+  self.signature['AnatImage'].userLevel = 3
   
 def execution(self, context):
   context.write('Create subject hierarchy and convert image to mgz format.')
-  cmd = 'recon-all -i %s -subjid %s'%(self.RawT1Image.fullPath(), self.subjectName)
-  context.write(cmd)
-  context.write(self.database)
-  launchFreesurferCommand(context, self.database, 'recon-all', '-i', self.RawT1Image.fullPath(), '-subjid', self.subjectName)
+  launchFreesurferCommand( context, self.database, 'mri_convert', self.RawT1Image, self.AnatImage )
   createdDir = self.database+'/'+self.subjectName
   context.write("Updating database, path = " + createdDir)
   neuroHierarchy.databases.update([createdDir])
-  #neuroHierarchy.databases.update()
   
 
 
