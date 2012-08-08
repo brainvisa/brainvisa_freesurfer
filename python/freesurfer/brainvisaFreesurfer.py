a1a75d80a2d38e279f64afcf6fce62bbdb25994f
denisri <denis.riviere.gm@gmail.com>
2012-08-08 08:36:18 +0000

    force raising a ValidationError if testFreesurferCommand() fails.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/4.3@50605 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/freesurfer/brainvisaFreesurfer.py
+++ python/freesurfer/brainvisaFreesurfer.py
@@ -16,14 +16,13 @@ __freeSurferOK__ = None
 def testFreesurferCommand( force_retest=False ):
   global __freeSurferOK__
   if force_retest or __freeSurferOK__ is None:
-    #print " -- Function testFreesurferCommand -- "
     context = brainvisa.processes.defaultContext()
     try:
       launchFreesurferCommand(context, None, 'mri_convert', '-h')
       __freeSurferOK__ = True
     except:
       __freeSurferOK__ = False
-      raise
+      raise ValidationError( 'FreeSurfer not available.' )
 
 
 def launchFreesurferCommand( context, database=None, *args, **kwargs ):
@@ -61,33 +60,12 @@ def launchFreesurferCommand( context, database=None, *args, **kwargs ):
   cmdFreeSurferSystem = distutils.spawn.find_executable("mri_convert")
   if distutils.spawn.find_executable("mri_convert") :
     context.system ( * args,  **kwargs )
-  else:  
+  else:
     setupShell.append(configuration.freesurfer.freesurfer_home_path + "/FreeSurferEnv.sh")
     argShell = tuple(setupShell) + args
-    #print args
-    #print kwargs
-  
-    #freesurfer exec
-    #print "Test avec exec"
-    #setupExec = []
-    #setupExec.append(configuration.freesurfer.executable_freesurfer)
-    #print setupExec
-    #argExec = tuple(setupExec) + args
-    #print argExec
-  
-    #print "ArgShell"
-    #print argShell
-  
-    #try :  
-      #context.system ( *( (runFreesurferCommandSh, ) + argShell ),  **kwargs )
-    #except:
-      #try: 
-        #context.system ( *( (runFreesurferCommandSh, ) + argExec ),  **kwargs )
-      #except:  
-        #raise ValidationError( 'FreeSurfer not available' )
 
-    try :  
+    try :
       context.system ( *( (runFreesurferCommandSh, ) + argShell ),  **kwargs )
-    except:  
+    except:
       raise ValidationError( 'FreeSurfer not available or one freesurfer command line has failed. Please see the log file in the main menu of BrainVISA for more information.' )
-  
+
