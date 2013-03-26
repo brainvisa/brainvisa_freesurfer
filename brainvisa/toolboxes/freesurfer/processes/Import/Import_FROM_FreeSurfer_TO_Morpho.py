327653d7479885c5ff44e5b76d0eb370a333f32a
denisri <denis.riviere.gm@gmail.com>
2013-03-26 14:02:46 +0000

    added necessary parameters on FS/Morphologist conversion pipeline to worin in distributed mode. Fixes: #7323
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@53880 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Import/Import_FROM_FreeSurfer_TO_Morpho.py
+++ brainvisa/toolboxes/freesurfer/processes/Import/Import_FROM_FreeSurfer_TO_Morpho.py
@@ -36,8 +36,7 @@ from brainvisa.validation import ValidationError
 import shfjGlobals, stat
 from soma import aims
 import numpy
-import registration
-#from freesurfer.brainvisaFreesurfer import launchFreesurferCommand
+from brainvisa import registration
 from freesurfer.brainvisaFreesurfer import *
 import threading 
 from soma.wip.application.api import Application
@@ -47,24 +46,12 @@ from soma.wip.application.api import Application
 configuration = Application().configuration
 
 
- 
-#def validation():
-#  pass
-   #if ( not configuration.SPM.spm8_standalone_command \
-       #or not configuration.SPM.spm8_standalone_mcr_path ) \
-       #or not distutils.spawn.find_executable( \
-          #configuration.matlab.executable ):
-          #raise ValidationError( 'SPM or matlab is not found' )
-
-
 def delInMainThread( lock, thing ): #Pour pb de communiation avec Anatomist
   lock.acquire()
   del thing
-  #print 'deleted'
   lock.release()
 
-
-
+  
 name = 'Import From FreeSurfer to T1 pipeline'
 roles = ('importer',)
 userLevel = 1
@@ -72,77 +59,86 @@ userLevel = 1
 
 def validation():
   pass
-  #try:
-    #from soma import aims
-  #except:
-    #raise ValidationError( 'aims module not available' )
-  #try:
-    #import numpy
-  #except:
-    #raise ValidationError( 'numpy module not available' )
-
-  #testFreesurferCommand()
-  #try: 
-    #testFreesurferCommand(self.context)
-    #print "retour testFreesurferCommand : "
-    #print retour
-  #except:
-    #raise ValidationError( 'FreeSurfer not available' )
-  
 
 signature=Signature(
   'T1_orig', ReadDiskItem( 'T1 FreesurferAnat',  'FreesurferMGZ', exactType=True ),
   #seems no mandatory ?
-  #'nu_image', ReadDiskItem( 'Nu FreesurferAnat', 'FreesurferMGZ' ),
   'ribbon_image', ReadDiskItem( 'Ribbon Freesurfer', 'FreesurferMGZ' ),
   'Talairach_Auto', ReadDiskItem( 'Talairach Auto Freesurfer', 'MINC transformation matrix' ), 
- # 'normalized_referential', ReadDiskItem( 'Referential', 'Referential'),
   'T1_output', WriteDiskItem( 'Raw T1 MRI', [ 'GIS image', 'NIFTI-1 image', 'gz compressed NIFTI-1 image' ] ),
+  'T1_referential', WriteDiskItem( 'Referential of Raw T1 MRI', 'Referential' ),
   'Biais_corrected_output', WriteDiskItem( 'T1 MRI Bias Corrected', [ 'GIS image', 'NIFTI-1 image', 'gz compressed NIFTI-1 image' ] ),
   'normalization_transformation',  WriteDiskItem( 'Transform Raw T1 MRI to Talairach-MNI template-SPM', 'Transformation matrix' ), 
-  'Talairach_transform',  WriteDiskItem( 'Transform Raw T1 MRI to Talairach-AC/PC-Anatomist', 'Transformation matrix' ), 
+  'Talairach_transform',  WriteDiskItem( 'Transform Raw T1 MRI to Talairach-AC/PC-Anatomist', 'Transformation matrix' ),
+  'commissure_coordinates', WriteDiskItem( 'Commissure Coordinates', 'Commissure coordinates' ),
   'histo_analysis', WriteDiskItem( 'Histo Analysis', 'Histo Analysis' ),
-  #'hfiltered', WriteDiskItem( "T1 MRI Filtered For Histo", shfjGlobals.aimsWriteVolumeFormats ),
-  #'white_ridges', WriteDiskItem( "T1 MRI White Matter Ridges",  shfjGlobals.aimsWriteVolumeFormats ),
   'Split_brain_output', WriteDiskItem( 'Split Brain Mask', [ 'GIS image', 'NIFTI-1 image', 'gz compressed NIFTI-1 image' ] ),
   'Rgrey_white_output', WriteDiskItem( 'Right Grey White Mask', [ 'GIS image', 'NIFTI-1 image', 'gz compressed NIFTI-1 image' ] ),
   'Lgrey_white_output', WriteDiskItem( 'Left Grey White Mask', [ 'GIS image', 'NIFTI-1 image', 'gz compressed NIFTI-1 image' ] ),
-  # 'input_spm_orientation', Choice( '0', '1', ), 
-  #Obsolete with Morphologist 2012
-  #'left_hemi_cortex', WriteDiskItem( 'Left CSF+GREY Mask',
-  #    'Aims writable volume formats' ),
-  #'right_hemi_cortex', WriteDiskItem( 'Right CSF+GREY Mask',
-  #    'Aims writable volume formats' ), 
   'use_t1pipeline', Choice( ( 'graphically', 0 ), ( 'in batch', 1 ), ( 'don\'t use it', 2 ) ),
-  #'database', ReadDiskItem( 'Directory', 'Directory' ),
+  'mni_referential', ReadDiskItem( 'Referential', 'Referential' ),
+  'transform_chain_ACPC_to_Normalized',  ListOf( ReadDiskItem( 'Transformation', 'Transformation matrix' ) ),
+  'acpc_referential', ReadDiskItem('Referential', 'Referential'),
+  'bias_field', WriteDiskItem( 'T1 MRI bias field', 'aims writable volume formats' ),
+  'hfiltered', WriteDiskItem( 'T1 MRI filtered for histo', 'aims writable volume formats' ),
+  'white_ridges', WriteDiskItem( 'T1 MRI white matter ridges', 'aims writable volume formats', exactType=True ),
+  'mean_curvature', WriteDiskItem( 'T1 MRI mean curvature', 'aims writable volume formats' ),
+  'variance', WriteDiskItem( 'T1 MRI variance', 'aims writable volume formats' ),
+  'edges', WriteDiskItem( 'T1 MRI edges', 'aims writable volume formats' ),
 )
 
 
 def initialization( self ):
-  #def linkDB( self, dummy ):
-    #if self.T1_orig:
-      #return self.T1_orig.get('_database')
+
+  def linkACPC_to_norm( proc, param ):
+    trManager = registration.getTransformationManager()
+    if proc.mni_referential:
+      try:
+        id = proc.mni_referential.uuid()
+      except:
+        return []
+      _mniToACPCpaths = trManager.findPaths( \
+        registration.talairachACPCReferentialId, id )
+      for x in _mniToACPCpaths:
+        return x
+      else:
+        return []
 
   #self.signature[ 'output' ].browseUserLevel = 3
   #self.signature[ 'nu input' ].databaseUserLevel = 2
-  #self.linkParameters( 'nu_image', 'T1_orig' )
   self.linkParameters( 'ribbon_image', 'T1_orig' )
   self.linkParameters( 'Talairach_Auto', 'T1_orig' )
   self.linkParameters( 'Biais_corrected_output', 'T1_output' )
-  #self.linkParameters( 'white_ridges', 'T1_output' )
-  #self.linkParameters( 'hfiltered', 'T1_output' )
   self.linkParameters( 'Split_brain_output', 'T1_output' )
   self.linkParameters( 'normalization_transformation', 'T1_output' )
   self.linkParameters( 'Talairach_transform', 'T1_output' )
   self.linkParameters( 'histo_analysis', 'Biais_corrected_output' )
-  #self.linkParameters( 'histo_analysis', 'T1_output' )
   self.linkParameters( 'Rgrey_white_output', 'T1_output' )
   self.linkParameters( 'Lgrey_white_output', 'T1_output' )
-  #Obsolete with Morphologist 2012
-  #self.linkParameters( 'left_hemi_cortex', 'Biais_corrected_output' )
-  #self.linkParameters( 'right_hemi_cortex', 'Biais_corrected_output' )
-  #self.linkParameters('database', 'T1_orig', linkDB )
-  #self.signature[ 'database' ].userLevel = 2
+  self.linkParameters( 'T1_referential', 'T1_output' )
+  self.linkParameters( 'commissure_coordinates', 'T1_output' )
+  self.linkParameters( 'transform_chain_ACPC_to_Normalized',
+    'mni_referential', linkACPC_to_norm )
+  trManager = registration.getTransformationManager()
+  self.mni_referential = trManager.referential(
+    registration.talairachMNIReferentialId )
+  self.acpc_referential = trManager.referential(
+    registration.talairachACPCReferentialId )
+  self.linkParameters( 'bias_field', 'T1_output' )
+  self.linkParameters( 'hfiltered', 'T1_output' )
+  self.linkParameters( 'white_ridges', 'T1_output' )
+  self.linkParameters( 'mean_curvature', 'T1_output' )
+  self.linkParameters( 'variance', 'T1_output' )
+  self.linkParameters( 'edges', 'T1_output' )
+  self.signature['mni_referential'].userLevel = 2
+  self.signature['transform_chain_ACPC_to_Normalized'].userLevel = 2
+  self.signature['acpc_referential'].userLevel = 2
+  self.signature['bias_field'].userLevel = 2
+  self.signature['hfiltered'].userLevel = 2
+  self.signature['white_ridges'].userLevel = 2
+  self.signature['mean_curvature'].userLevel = 2
+  self.signature['variance'].userLevel = 2
+  self.signature['edges'].userLevel = 2
 
 
 def execution( self, context ):
@@ -153,63 +149,28 @@ def execution( self, context ):
 
   #Temporary files
   tmp_ori = context.temporary( 'NIFTI-1 image', 'Raw T1 MRI'  )
-  #tmp_nu = context.temporary( 'NIFTI-1 image', 'T1 MRI Bias Corrected'  )
   tmp_ribbon = context.temporary( 'NIFTI-1 image', 'Split Brain Mask'  )
   database = ''
-  
+
   #Convert the three volumes from .mgz to .nii with Freesurfer
   context.write("Convert .mgz to .nii with FreeSurfer")
 
   #correct line
-  launchFreesurferCommand(context, database, 'mri_convert', '-i', self.T1_orig, '-o', tmp_ori)
-  
-  #launchFreesurfer(context, database, 'mri_convert', '-i', self.T1_orig, '-o', tmp_ori)
-  #cmd = 'freesurfer | mri_convert -i /volatile/TMP/test_config_freesurfer/T1.mgz -o /volatile/TMP/test_config_freesurfer/result_test.nii'
-  #context.system(cmd)
-  
-  
-  #launchFreesurferCommand(context, database, 'mri_convert', '-i', self.nu_image, '-o', tmp_nu)
-  launchFreesurferCommand(context, database, 'mri_convert', '-i', self.ribbon_image, '-o', tmp_ribbon)
+  launchFreesurferCommand(context, database, 'mri_convert', '-i', self.T1_orig,
+    '-o', tmp_ori)
+
+  launchFreesurferCommand(context, database, 'mri_convert',
+    '-i', self.ribbon_image, '-o', tmp_ribbon)
 
   #Import Data 
   context.write("Import Data into database with brainvisa ontology")
   context.write(database)
-  #context.runProcess( 'ImportT1MRI', input=self.T1_orig, output=self.T1_output)
   context.runProcess( 'ImportT1MRI', input=tmp_ori, output=self.T1_output)
-  #context.runProcess( 'ImportGenericVolume', self.nu_image , self.Biais_corrected_output)
-  
-  #context.runProcess( 'ImportData', tmp_nu , self.Biais_corrected_output)
-  #context.system( 'AimsFileConvert', '-i',  self.Biais_corrected_output, '-o', self.Biais_corrected_output, '-t', 'S16')
-  
-  #context.runProcess( 'ImportGenericVolume', tmp_ribbon , self.Split_brain_output)
-  context.runProcess( 'ImportData', tmp_ribbon , self.Split_brain_output)
-  context.system( 'AimsFileConvert', '-i',  self.Split_brain_output, '-o', self.Split_brain_output, '-t', 'S16')
-  
-  
-  ##Convert .xfm and create ACPC file
-  ##mniReferential = trManager.referential(registration.talairachMNIReferentialId )
-  #if self.Talairach_Auto is not None:
-    ## import / convert transformation to MNI space
-    ##context.write( _t_( 'import transformation' ) )
-    #context.write("Convert Talairach_Auto into AC-PC File")
-    #m = []
-    #i = 0
-    #rl = False 
-    #for l in open( self.Talairach_Auto.fullPath() ).xreadlines():
-      #print l
-      #if l.startswith( 'Linear_Transform =' ):
-	#rl = True
-      #elif rl:
-	#if l.endswith( ';\n' ):
-	  #l = l[:-2]
-	  #print l
-	#m.append( [ float(x) for x in l.split() ] )
-	#i += 1
-	#if i == 3:
-	  #break
 
-  #Convert .xfm and create ACPC file
-  #mniReferential = trManager.referential(registration.talairachMNIReferentialId )
+  context.runProcess( 'ImportData', tmp_ribbon , self.Split_brain_output)
+  context.system( 'AimsFileConvert', '-i',  self.Split_brain_output,
+    '-o', self.Split_brain_output, '-t', 'S16')
+
   if self.Talairach_Auto is not None:
     # import / convert transformation to MNI space
     #context.write( _t_( 'import transformation' ) )
@@ -227,11 +188,9 @@ def execution( self, context ):
         i += 1
         if i == 3:
           break
-            
-            
-            
-    talairach_freesrufer = aims.AffineTransformation3d( numpy.array( m  + [[ 0., 0., 0., 1. ]] ) )
-    #print shfjGlobals.aimsVolumeAttributes( tmp_ori)[ 'transformations' ]
+
+    talairach_freesrufer = aims.AffineTransformation3d(
+      numpy.array( m  + [[ 0., 0., 0., 1. ]] ) )
     header_nifti =  aims.AffineTransformation3d(shfjGlobals.aimsVolumeAttributes(tmp_ori)[ 'transformations' ][-1] )
     t1aims2mni = talairach_freesrufer * header_nifti
     aims.write( t1aims2mni, self.normalization_transformation.fullPath() )
@@ -239,11 +198,17 @@ def execution( self, context ):
     if self.Talairach_transform is not None:
       trm = context.temporary( 'Transformation matrix' )
       aims.write( t1aims2mni, trm.fullPath() )
-      
-      context.runProcess( 'TalairachTransformationFromNormalization', self.normalization_transformation,  self.T1_output, self.Talairach_transform, self.T1_output, self.T1_output)
+
+      context.runProcess( 'TalairachTransformationFromNormalization',
+        normalization_transformation=self.normalization_transformation,
+        Talairach_transform=self.Talairach_transform,
+        commissure_coordinates=self.commissure_coordinates,
+        t1mri=self.T1_output, source_referential=self.T1_referential,
+        normalized_referential=self.mni_referential,
+        transform_chain_ACPC_to_Normalized=self.transform_chain_ACPC_to_Normalized,
+        acpc_referential=self.acpc_referential )
 
 
-  #VipGreyStatClassif  = self.Split_brain_output
   #change labels for Split Brain
   context.write("Create R/L-Grey white files from ribbon freesurfer data")
   VipGreyStatClassif = context.temporary( 'NIFTI-1 image' )
@@ -262,49 +227,20 @@ def execution( self, context ):
 
   #Launch VipT1BiaisCorrection
   context.write("Launch T1BiasCorrection")
-  
 
-  #On doit indiquer les valeurs de write_hfiltered et write_wridges à no maintenant ?
-  context.runProcess( 'T1BiasCorrection', mri=self.T1_output, mri_corrected=self.Biais_corrected_output, commissure_coordinates=self.Talairach_transform)
+  context.runProcess( 'T1BiasCorrection', mri=self.T1_output,
+    mri_corrected=self.Biais_corrected_output,
+    field=self.bias_field,
+    hfiltered=self.hfiltered,
+    white_ridges=self.white_ridges,
+    meancurvature=self.mean_curvature,
+    variance=self.variance,
+    edges=self.edges,
+    commissure_coordinates=self.commissure_coordinates)
 
   #Launch VipGreyStatFromClassif to generate a histo analysis file
   context.write("Launch VipGreyStatFromClassif to generate a histo analysis file")
   context.system( 'VipGreyStatFromClassif', '-i',  self.Biais_corrected_output, '-c', VipGreyStatClassif, '-a', self.histo_analysis, '-g', '100', '-w','200')
-
-  
-
-  #Obsolete with Morphologist 2012
-  ##Histo temporaire pour segmentaiton du cortex
-  #han = context.temporary( 'Histo Analysis' )
-  #open( han.fullPath(), 'w' ).write( \
-  #'''sequence: unknown
-  #csf: mean: -1 sigma: -1
-  #gray: mean: 100 sigma: 1
-  #white: mean: 200 sigma: 1
-  #''' )
-  
-  
-  #Obsolete with Morphologist 2012
-  ##Segmentation du cortex : Lcortex_subject
-  #Lbraing = context.temporary( 'GIS Image' )
-  #context.system( 'VipMask', '-i', self.Lgrey_white_output, "-m",
-                  #self.Split_brain_output, "-o", Lbraing, "-w",
-                  #"t", "-l", "2" )
-  #context.system( "VipHomotopicSnake", "-i", Lbraing, "-h",
-                  #han, "-o", self.left_hemi_cortex, "-w", "t" )
-  #trManager.copyReferential(self.Biais_corrected_output, self.left_hemi_cortex)
-  
-  
-  ##Segmentation du cortex : Rcortex_subject
-  #Rbraing = context.temporary( 'GIS Image' )
-  #context.system( "VipMask", "-i", self.Rgrey_white_output, "-m",
-                  #self.Split_brain_output, "-o", Rbraing,
-                  #"-w", "t", "-l", "1" )
-  #context.system( "VipHomotopicSnake", "-i", Rbraing, "-h",
-                  #han, "-o", self.right_hemi_cortex, "-w", "t" )
-  #trManager.copyReferential(self.Biais_corrected_output, self.right_hemi_cortex)
-  #end of obsolete with Morphologist 2012
-
 
   #Lock Data
   self.T1_output.lockData()
@@ -315,53 +251,36 @@ def execution( self, context ):
   self.Split_brain_output.lockData()
   self.Rgrey_white_output.lockData()
   self.Lgrey_white_output.lockData()
-  #Obsolete with Morphologist 2012
-  #self.left_hemi_cortex.lockData()
-  #self.right_hemi_cortex.lockData()
  
 
 
   #Launch Morphologist
-  t1pipeline = getProcessInstance( 'morphologist' )
-  t1pipeline.mri = self.T1_output
-  
-  t1pipeline.mri_corrected = self.Biais_corrected_output
-  
-  enode = t1pipeline.executionNode()
-  
-  #npi, proc = context.getProgressInfo( enode, parent=pi )
-  
-  #context.progress()
+  if self.use_t1pipeline != 2:
+    t1pipeline = getProcessInstance( 'morphologist' )
+    t1pipeline.mri = self.T1_output
+
+    t1pipeline.mri_corrected = self.Biais_corrected_output
+
+    enode = t1pipeline.executionNode()
+
+    context.write( _t_( 'Now run the last part of the regular T1 pipeline.' ) )
 
 
-  context.write( _t_( 'Now run the last part of the regular T1 pipeline.' ) )
-  
-  
-  enode.PrepareSubject.setSelected( False )
-  enode.BiasCorrection.setSelected( False )
-  enode.HistoAnalysis.setSelected( False )
-  enode.BrainSegmentation.setSelected( False )
-  enode.SplitBrain.setSelected( False )
-  enode.TalairachTransformation.setSelected( False )
-  
-  #enode.GreyWhiteInterface.setSelected( True )
-  #enode.GreyWhiteInterface.GreyWhiteInterface.setSelected( False )
-  #For Morphologist 2012
-  enode.GreyWhiteClassification.setSelected( False )
-  #enode.GreyWhiteInterface.cortex_image.setSelected( False )
-  #enode.GreyWhiteInterface.GreyWhiteMesh.GreyWhiteInterface05.setSelected( True )
-  #For Morphologist 2012
-  enode.GreyWhiteSurface.setSelected( True )
-  
-  enode.HemispheresMesh.setSelected( True )
-  enode.HeadMesh.setSelected( True )
-  enode.CorticalFoldsGraph.setSelected( True )
-  # we _must_ build 3.1 graphs because 3.0 overwrite cortex images
-  # now we could use 3.0 graph thanks to Morpho 2012 but it seems a good choice to keep 
-  #3.1 even if it is not the default value in Mropho 2012
-  enode.CorticalFoldsGraph.CorticalFoldsGraph_3_1.setSelected( True )
-
-
+    enode.PrepareSubject.setSelected( False )
+    enode.BiasCorrection.setSelected( False )
+    enode.HistoAnalysis.setSelected( False )
+    enode.BrainSegmentation.setSelected( False )
+    enode.SplitBrain.setSelected( False )
+    enode.TalairachTransformation.setSelected( False )
+    enode.GreyWhiteClassification.setSelected( False )
+    enode.GreyWhiteSurface.setSelected( True )
+    enode.HemispheresMesh.setSelected( True )
+    enode.HeadMesh.setSelected( True )
+    enode.CorticalFoldsGraph.setSelected( True )
+    # we _must_ build 3.1 graphs because 3.0 overwrite cortex images
+    # now we could use 3.0 graph thanks to Morpho 2012 but it seems a good choice to keep
+    #3.1 even if it is not the default value in Mropho 2012
+    enode.CorticalFoldsGraph.CorticalFoldsGraph_3_1.setSelected( True )
 
   if self.use_t1pipeline == 0:
     pv = mainThreadActions().call( ProcessView, t1pipeline )
@@ -381,15 +300,7 @@ def execution( self, context ):
     context.write( '<font color="#a0a060">' \
       + _t_( 'Pipeline not run since the "use_t1pipeline" parameter ' \
         'prevents it' ) + '</font>')
-  
+
   context.write( 'OK')
   #context.progress( 10, nsteps, self )
-  
-
-
-
-
-
-
-
 
