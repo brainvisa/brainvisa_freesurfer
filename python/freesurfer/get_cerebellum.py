4a1edf40cd385b21524e51778aaad8e1f9a6e2af
lefranc <lefranc@bioproj.invalid>
2013-03-27 08:47:53 +0000

    can be used to add the cerebellum from segmentation of hemisphere
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@53892 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ python/freesurfer/get_cerebellum.py
@@ -0,0 +1,25 @@
+from soma import aims
+import numpy
+import sys
+
+infile = sys.argv[1]
+splitname = sys.argv[2]
+outfile = sys.argv[3]
+
+vol = aims.read( infile )
+dat = numpy.asarray( vol )
+dat[dat==3] = 0
+dat[dat==7] = 3
+dat[dat==8] = 3
+dat[dat==15] = 3
+dat[dat==16] = 3
+dat[dat==46] = 3
+dat[dat==47] = 3
+dat[dat!=3] = 0
+split = aims.read( splitname )
+sdat = numpy.asarray( split )
+dat[ sdat != 0 ] = 0
+sdat += dat
+print split
+aims.write( split, outfile )
+
