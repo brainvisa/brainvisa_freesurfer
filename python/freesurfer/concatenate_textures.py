eb95ecbf995bccb09db04c992c5b4e2bb01f5990
lefranc <lefranc@bioproj.invalid>
2013-12-09 08:50:30 +0000

    some errors in the labels numbering, left hemisphere had the same number as the right hemisphere.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@59884 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/freesurfer/concatenate_textures.py
+++ python/freesurfer/concatenate_textures.py
@@ -1,33 +1,38 @@
 from numpy import array
+import numpy
 #from tio import Texture
 from soma import aims
 import sys
 
 def usage():
-    print "concatenate textures"
-    #print "usage: python concatenate_textures.py output.tex file1.tex ... fileN.tex"
+  print "concatenate textures"
+  #print "usage: python concatenate_textures.py output.tex file1.tex ... fileN.tex"
 
-def concatenate_textures(output, fileG, fileD): 
-    gyriTexR = aims.read(fileD)
-    gyriTexL = aims.read(fileG)
-    gyriTexB = aims.TimeTexture_S16()
-    vertexNbR = gyriTexR[0].nItem()
-    vertexNbL = gyriTexL[0].nItem()
-    vertexNbB = vertexNbR + vertexNbL
-    gyriTexB[0].reserve(vertexNbB)
-    for v in xrange(vertexNbL):
-       gyriTexB[0].push_back(gyriTexL[0][v]+1)
-       lh_max = gyriTexB[0].arraydata().max()
-    for v in xrange(vertexNbR):
-       gyriTexB[0].push_back(gyriTexR[0][v]+1 + lh_max)
-    aims.write(gyriTexB, output)
+def concatenate_textures(output, fileL, fileR): 
+  gyriTexR = aims.read(fileR)
+  gyriTexL = aims.read(fileL)
+  gyriTexB = aims.TimeTexture_S16()
+  vertexNbR = gyriTexR[0].nItem()
+  vertexNbL = gyriTexL[0].nItem()
+  vertexNbB = vertexNbR + vertexNbL
+  gyriTexB[0].reserve(vertexNbB)
+  for v in xrange(vertexNbL):
+    gyriTexB[0].push_back(gyriTexL[0][v] + 1)
+    lh_max = gyriTexB[0].arraydata().max()
+  gyriTexB[0].arraydata()[gyriTexB[0].arraydata() == 0] = 1
+  for v in xrange(vertexNbR):
+    value = gyriTexR[0][v] + 1 + lh_max
+    if value == lh_max:
+      value = value + 1
+    gyriTexB[0].push_back(value)
+  aims.write(gyriTexB, output)
 
 if __name__ == "__main__":
-    if len(sys.argv)<=3:
-        usage()
-        sys.exit(1)
-    output = sys.argv[1]
-    fileG = sys.argv[2]
-    fileD = sus.argv[3]
-    concatenate_textures(output, fileG, fileD)
+  if len(sys.argv)<=3:
+    usage()
+    sys.exit(1)
+  output = sys.argv[1]
+  fileL = sys.argv[2]
+  fileR = sus.argv[3]
+  concatenate_textures(output, fileL, fileR)
 
