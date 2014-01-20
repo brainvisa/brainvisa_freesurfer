163faa5654f89641039d7c7491e0bcd8d762f8bd
lefranc <lefranc@bioproj.invalid>
2014-01-20 17:28:22 +0000

    fix import
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@60447 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- bin/constelAvgGyriTexture.py
+++ bin/constelAvgGyriTexture.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
-from soma import aims
 import optparse
 import sys
-
-def validation():
-  try:
-    import constel
-  except:
-    raise ValidationError( 'constellation module is not here.' )
-
-import constel.lib.texturetools as TT
+import soma.aims.texturetools as satt
 
 def parseOpts( argv ):
-  description = 'Create average texture labels. usage : python average_texture_labels.py output.gii(.tex) subject1.gii(.tex) ... subjectN.gii(.tex)'
+  description = 'Create average gyri texture. usage : python average_texture.py output.gii(.tex) subject1.gii(.tex) ... subjectN.gii(.tex)'
   parser = optparse.OptionParser( description )
   parser.add_option('-i', '--itex', dest = 'itex',
     metavar = 'FILE', action='append',
@@ -24,6 +16,6 @@ def parseOpts( argv ):
 
 def main():
   parser, ( options, args ) = parseOpts( sys.argv )
-  TT.average_texture_labels( options.otex, options.itex )
+  satt.average_texture( options.otex, options.itex )
 
 if __name__ == "__main__" : main()
