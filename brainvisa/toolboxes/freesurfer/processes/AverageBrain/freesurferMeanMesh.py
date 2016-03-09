92f48d3ec130e56f175d4338f055bbce9f8794e1
lefranc <lefranc@bioproj.invalid>
2016-03-09 12:53:18 +0000

    AverageBrain: merge changes of bug_fix to trunk
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@72071 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/AverageBrain/freesurferMeanMesh.py
+++ brainvisa/toolboxes/freesurfer/processes/AverageBrain/freesurferMeanMesh.py
@@ -1,49 +1,118 @@
-# -*- coding: utf-8 -*-
-from brainvisa.processes import *
+###############################################################################
+# This software and supporting documentation are distributed by CEA/NeuroSpin,
+# Batiment 145, 91191 Gif-sur-Yvette cedex, France. This software is governed
+# by the CeCILL license version 2 under French law and abiding by the rules of
+# distribution of free software. You can  use, modify and/or redistribute the
+# software under the terms of the CeCILL license version 2 as circulated by
+# CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+###############################################################################
+
+"""
+This script does the following:
+* provides create left, right and both average mesh from a group of subjects.
+
+Main dependencies: axon python API.
+"""
+
+#----------------------------Imports-------------------------------------------
+
+
+# axon python API modules
+from brainvisa.processes import Signature, ReadDiskItem, WriteDiskItem
 from brainvisa.group_utils import Subject
+
+# soma module
 from soma.minf.api import registerClass, readMinf
 
-name = '2 Average brain mesh'
+
+#----------------------------Header--------------------------------------------
+
+
+name = "2 Average brain mesh"
 userLevel = 1
 
 signature = Signature(
-  'group', ReadDiskItem('Freesurfer Group definition', 'XML' ),
-  'LeftAverageMesh', WriteDiskItem('AverageBrainWhite', 'MESH mesh',
-                                   requiredAttributes = {'side':'left'}),
-  'RightAverageMesh', WriteDiskItem('AverageBrainWhite', 'MESH mesh',
-                                   requiredAttributes = {'side':'right'}),
-  'BothAverageMesh', WriteDiskItem('BothAverageBrainWhite', 'MESH mesh'),
-);
+    # input
+    "group", ReadDiskItem("Freesurfer Group definition", "XML"),
+
+    # outputs
+    "LeftAverageMesh", WriteDiskItem("AverageBrainWhite", "MESH mesh",
+                                     requiredAttributes={"side": "left"}),
+    "RightAverageMesh", WriteDiskItem("AverageBrainWhite", "MESH mesh",
+                                      requiredAttributes={"side": "right"}),
+    "BothAverageMesh", WriteDiskItem("BothAverageBrainWhite", "MESH mesh"),
+)
+
+
+#----------------------------Functionc-----------------------------------------
+
 
 def initialization(self):
-  self.linkParameters('LeftAverageMesh', 'group')
-  self.linkParameters('RightAverageMesh', 'group')
-  self.linkParameters('BothAverageMesh', 'group')
+    """
+    """
+    self.linkParameters("LeftAverageMesh", "group")
+    self.linkParameters("RightAverageMesh", "group")
+    self.linkParameters("BothAverageMesh", "group")
+
 
 def execution(self, context):
-  registerClass('minf_2.0', Subject, 'Subject')
-  groupOfSubjects = readMinf(self.group.fullPath())
+    """
+    """
+    registerClass("minf_2.0", Subject, "Subject")
+    groupOfSubjects = readMinf(self.group.fullPath())
 
-  # Left side
-  subjects = []
-  rattrs = {'side' : 'left'}
-  for subject in groupOfSubjects:
-    subjects.append(ReadDiskItem('AimsWhite','MESH mesh').findValue(subject.attributes(), requiredAttributes = rattrs))
+    ###########################################################################
+    #                        LEFT HEMISPHERE (lh)                             #
+    ###########################################################################
 
-  context.write(str([i.fullPath() for i in subjects]))
+    # list all left meshes
+    subjects = []
+    rattrs = {"side": "left"}
+    for subject in groupOfSubjects:
+        subjects.append(
+            ReadDiskItem("AimsWhite", "MESH mesh").findValue(
+                subject.attributes(), requiredAttributes=rattrs))
 
-  context.system('python', '-c', 'from freesurfer.average_mesh import average_mesh as f; f(\"%s\", %s);'%(self.LeftAverageMesh.fullPath(), str([i.fullPath() for i in subjects])))
+    context.write(str([i.fullPath() for i in subjects]))
 
-  # Right side
-  subjects = []
-  rattrs = {'side' : 'right'}
-  for subject in groupOfSubjects:
-    subjects.append(ReadDiskItem('AimsWhite','MESH mesh').findValue(subject.attributes(), requiredAttributes = rattrs))
+    # create the left average mesh
+    context.system(
+        "python",
+        "-c",
+        "from freesurfer.average_mesh import average_mesh as f; f(\"%s\", %s);"%(
+            self.LeftAverageMesh.fullPath(),
+            str([i.fullPath() for i in subjects])))
 
-  context.write(str([i.fullPath() for i in subjects]))
+    ###########################################################################
+    #                        RIGHT HEMISPHERE (rh)                            #
+    ###########################################################################
 
-  context.system('python', '-c', 'from freesurfer.average_mesh import average_mesh as f; f(\"%s\", %s);'%(self.RightAverageMesh.fullPath(), str([i.fullPath() for i in subjects])))
-  
-  context.system( 'AimsZCat', '-i', self.LeftAverageMesh.fullPath(),
-                  self.RightAverageMesh.fullPath(), '-o',
-                  self.BothAverageMesh.fullPath() )
+    # list all right meshes
+    subjects = []
+    rattrs = {"side": "right"}
+    for subject in groupOfSubjects:
+        subjects.append(
+            ReadDiskItem("AimsWhite", "MESH mesh").findValue(
+                subject.attributes(), requiredAttributes=rattrs))
+
+    context.write(str([i.fullPath() for i in subjects]))
+
+    # create the right average mesh
+    context.system(
+        "python",
+        "-c",
+        "from freesurfer.average_mesh import average_mesh as f; f(\"%s\", %s);"%(
+            self.RightAverageMesh.fullPath(),
+            str([i.fullPath() for i in subjects])))
+
+    ###########################################################################
+    #                        BOTH HEMISPHERE (bh)                             #
+    ###########################################################################
+
+    # concatenate the left and right average meshes
+    # create the both average mesh
+    context.system(
+        "AimsZCat",
+        "-i", self.LeftAverageMesh.fullPath(), self.RightAverageMesh.fullPath(),
+        "-o", self.BothAverageMesh.fullPath()
+        )
