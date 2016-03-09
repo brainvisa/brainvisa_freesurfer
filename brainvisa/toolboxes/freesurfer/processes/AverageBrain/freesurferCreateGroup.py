d4a0fce8d67deafa07e5309d540e5264ed2a31a7
lefranc <lefranc@bioproj.invalid>
2016-03-09 12:51:35 +0000

    AverageBrain: code cleanup (pep8 and doc)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@72070 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/AverageBrain/freesurferCreateGroup.py
+++ brainvisa/toolboxes/freesurfer/processes/AverageBrain/freesurferCreateGroup.py
@@ -1,22 +1,41 @@
-# -*- coding: utf-8 -*-
-from brainvisa.processes import *
-from brainvisa.group_utils import Subject
-from soma.minf.api import registerClass, writeMinf
+###############################################################################
+# This software and supporting documentation are distributed by CEA/NeuroSpin,
+# Batiment 145, 91191 Gif-sur-Yvette cedex, France. This software is governed
+# by the CeCILL license version 2 under French law and abiding by the rules of
+# distribution of free software. You can  use, modify and/or redistribute the
+# software under the terms of the CeCILL license version 2 as circulated by
+# CEA, CNRS and INRIA at the following URL "http://www.cecill.info".
+###############################################################################
 
-name = '1 Creation of a group of subject'
+"""
+This script does the following:
+* provides create a FreeSurfer group.
+
+Main dependencies: axon python API.
+"""
+
+#----------------------------Imports-------------------------------------------
+
+
+# axon python API modules
+from brainvisa.processes import Signature, ListOf, ReadDiskItem, WriteDiskItem
+
+
+#----------------------------Header--------------------------------------------
+
+
+name = "1 Creation of a group of subject"
 userLevel = 1
 
 signature = Signature(
-  'list_of_subjects',ListOf ( ReadDiskItem("Subject", 'Directory') ),
-  'group_definition', WriteDiskItem('Freesurfer Group definition', 'XML' ),
-);
+    "list_of_subjects", ListOf(ReadDiskItem("Subject", "Directory")),
+    "group_definition", WriteDiskItem("Freesurfer Group definition", "XML"),
+)
 
-def initialization(self):
-  pass
 
+#----------------------------Functions-----------------------------------------
 
 
 def execution(self, context):
-  context.runProcess( 'createGroup', self.list_of_subjects,
-    self.group_definition )
-
+    context.runProcess(
+        "createGroup", self.list_of_subjects, self.group_definition)
