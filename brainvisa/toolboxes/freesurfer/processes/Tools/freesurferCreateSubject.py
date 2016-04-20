1d8bbb9f8d6befbae262452b6ef38637455951e9
lefranc <lefranc@bioproj.invalid>
2016-04-20 09:39:34 +0000

    freesurferCreateSubject/freesurferPipelineComplete: signature change ("NIFTI-1 image" to ["NIFTI-1 image", "GZ Compressed NIFTI-1 image"]) in order to inactivate AimsFileConvert; code cleanup (pep8)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@72708 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Tools/freesurferCreateSubject.py
+++ brainvisa/toolboxes/freesurfer/processes/Tools/freesurferCreateSubject.py
@@ -1,44 +1,61 @@
 # -*- coding: utf-8 -*-
+
 import os
-from brainvisa.processes import *
+from brainvisa.processes import (Signature, String, Choice, neuroHierarchy,
+                                 ReadDiskItem, WriteDiskItem, OpenChoice)
 from freesurfer.brainvisaFreesurfer import launchFreesurferCommand
 
 name = "01 Create Freesurfer subject from T1 anatomical image"
 userLevel = 1
 
 signature = Signature(
-  'RawT1Image', ReadDiskItem('Raw T1 MRI', 'NIFTI-1 image'),
-  'subjectName', String(),
-  'database', Choice(),
-  'AnatImage', WriteDiskItem( 'RawFreesurferAnat', 'FreesurferMGZ' )
-  )
+    "RawT1Image", ReadDiskItem(
+        "Raw T1 MRI", ["NIFTI-1 image", "GZ Compressed NIFTI-1 image"]),
+    "subjectName", String(),
+    "database", Choice(),
+    "AnatImage", WriteDiskItem("RawFreesurferAnat", "FreesurferMGZ")
+)
+
 
 def initialization(self):
-  def linkSubjectName( proc, dummy ):
-    if proc.RawT1Image is not None:
-      return os.path.basename( os.path.dirname( os.path.dirname( os.path.dirname( proc.RawT1Image.fullName() ) ) ) )
-  databases=[h.name for h in neuroHierarchy.hierarchies() if h.fso.name == 'freesurfer']
-  self.signature['database'].setChoices(*databases)
-  if len( databases ) != 0:
-    self.database=databases[0]
-  else:
-    self.signature[ 'database' ] = OpenChoice()
-  self.linkParameters( 'subjectName', 'RawT1Image', linkSubjectName )
-  def linkAnatImage( proc, dummy ):
-    if proc.subjectName is not None and proc.database is not None:
-      subject = proc.subjectName
-      dirname = proc.database
-      filename = os.path.join( dirname, subject, 'mri/orig/001.mgz' )
-      return filename
-  self.linkParameters( 'AnatImage', ( 'subjectName', 'database' ), linkAnatImage )
-  self.signature['AnatImage'].userLevel = 3
-  
+    """
+    """
+    databases = [h.name for h in neuroHierarchy.hierarchies()
+                 if h.fso.name == "freesurfer"]
+    self.signature["database"].setChoices(*databases)
+    if len(databases) != 0:
+        self.database = databases[0]
+    else:
+        self.signature["database"] = OpenChoice()
+
+    def linkSubjectName(proc, dummy):
+        """
+        """
+        if proc.RawT1Image is not None:
+            return os.path.basename(os.path.dirname(os.path.dirname(
+                os.path.dirname(proc.RawT1Image.fullName()))))
+
+    def linkAnatImage(proc, dummy):
+        """
+        """
+        if proc.subjectName is not None and proc.database is not None:
+            subject = proc.subjectName
+            dirname = proc.database
+            filename = os.path.join(dirname, subject, "mri/orig/001.mgz")
+            return filename
+
+    self.linkParameters("subjectName", "RawT1Image", linkSubjectName)
+    self.linkParameters(
+        "AnatImage", ("subjectName", "database"), linkAnatImage)
+    self.signature["AnatImage"].userLevel = 3
+
+
 def execution(self, context):
-  context.write('Create subject hierarchy and convert image to mgz format.') 
-  launchFreesurferCommand( context, self.database, 'mri_convert', self.RawT1Image, self.AnatImage )
-  createdDir = self.database+'/'+self.subjectName
-  context.write("Updating database, path = " + createdDir)
-  neuroHierarchy.databases.update([createdDir])
-  
-
-
+    """
+    """
+    context.write("Create subject hierarchy and convert image to mgz format.")
+    launchFreesurferCommand(context, self.database, "mri_convert",
+                            self.RawT1Image, self.AnatImage)
+    createdDir = self.database + "/" + self.subjectName
+    context.write("Updating database, path = " + createdDir)
+    neuroHierarchy.databases.update([createdDir])
--- brainvisa/toolboxes/freesurfer/processes/freesurferPipelineComplete.py
+++ brainvisa/toolboxes/freesurfer/processes/freesurferPipelineComplete.py
@@ -1,126 +1,135 @@
 # -*- coding: utf-8 -*-
-from brainvisa.processes import *
+from brainvisa.processes import (Signature, ReadDiskItem, Choice, OpenChoice,
+                                 SerialExecutionNode, ProcessExecutionNode,
+                                 neuroHierarchy)
 from freesurfer.brainvisaFreesurfer import testFreesurferCommand
 
-name = 'Freesurfer / BrainVisa full pipeline'
+name = "Freesurfer / BrainVisa full pipeline"
 userLevel = 1
 
+
 def validation():
-  testFreesurferCommand()
+    testFreesurferCommand()
 
 signature = Signature(
-  'RawT1Image', ReadDiskItem('Raw T1 MRI', 'NIFTI-1 image'),
-  'database', Choice(),
+    "RawT1Image", ReadDiskItem(
+        "Raw T1 MRI", ["NIFTI-1 image", "GZ Compressed NIFTI-1 image"]),
+    "database", Choice(),
 )
 
 
-def initialization( self ):
-  
-  eNode = SerialExecutionNode( self.name, parameterized=self )
-  
-  #01 Create Freesurfer subject from T1 anatomical image
-  eNode.addChild( 'FreeSurfer01',
-                  ProcessExecutionNode( 'freesurferCreateSubject',
-                  optional = 1 ) )
-                  
-  eNode.addDoubleLink( 'FreeSurfer01.RawT1Image',
-                       'RawT1Image' )
-  eNode.addDoubleLink( 'FreeSurfer01.database',
-                       'database' )
-  databases = [h.name for h in neuroHierarchy.hierarchies() if h.fso.name == 'freesurfer']
-  self.signature['database'].setChoices(*databases)
-  if len( databases ) != 0:
-    self.database = databases[0]
-  else:
-    self.signature[ 'database' ] = OpenChoice()
-  
-  #02 Launch Freesurfer full pipeline
-  eNode.addChild( 'FreeSurfer02',
-                  ProcessExecutionNode( 'freesurferPipeline',
-                  optional = 1 ) )
-                  
-  eNode.addDoubleLink( 'FreeSurfer02.AnatImage',
-                       'FreeSurfer01.AnatImage' )
-  
-  #Brainvisa Freesurfer pipeline
-  eNode.addChild( 'FreeSurferPipeline',
-                  ProcessExecutionNode( \
-                  'freesurferToBrainvisaConversionPipeline',
-                  optional = 1 ) )
-                  
-  eNode.addDoubleLink( 'FreeSurferPipeline.anat',
-                       'FreeSurfer02.AnatImage' )
-  
-  eNode.FreeSurferPipeline.removeLink('nu', 'anat')
-  eNode.FreeSurferPipeline.removeLink('ribbon', 'anat')
-                       
-  eNode.FreeSurferPipeline.removeLink('leftPial', 'anat')
-  eNode.FreeSurferPipeline.removeLink('leftCurv', 'anat')
-                                       
-  eNode.FreeSurferPipeline.removeLink('rightPial', 'anat')                                       
-  eNode.FreeSurferPipeline.removeLink('rightCurv', 'anat')
-  
-  eNode.addDoubleLink( 'FreeSurferPipeline.nu',
-                       'FreeSurfer02.nu' )
-  eNode.addDoubleLink( 'FreeSurferPipeline.ribbon',
-                       'FreeSurfer02.ribbon' )
-  
-  #left
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftPial',
-                       'FreeSurfer02.leftPial' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftWhite',
-                       'FreeSurfer02.leftWhite' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftSphereReg',
-                       'FreeSurfer02.leftSphereReg' )  
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftThickness',
-                       'FreeSurfer02.leftThickness' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftCurv',
-                       'FreeSurfer02.leftCurv' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftAvgCurv',
-                       'FreeSurfer02.leftAvgCurv' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftCurvPial',
-                       'FreeSurfer02.leftCurvPial' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftGyri',
-                       'FreeSurfer02.leftGyri' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.leftSulciGyri',
-                       'FreeSurfer02.leftSulciGyri' )
-                       
-  #right
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightPial',
-                       'FreeSurfer02.rightPial' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightWhite',
-                       'FreeSurfer02.rightWhite' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightSphereReg',
-                       'FreeSurfer02.rightSphereReg' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightThickness',
-                       'FreeSurfer02.rightThickness' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightCurv',
-                       'FreeSurfer02.rightCurv' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightAvgCurv',
-                       'FreeSurfer02.rightAvgCurv' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightCurvPial',
-                       'FreeSurfer02.rightCurvPial' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightGyri',
-                       'FreeSurfer02.rightGyri' )
-                       
-  eNode.addDoubleLink( 'FreeSurferPipeline.rightSulciGyri',
-                       'FreeSurfer02.rightSulciGyri' )
-  
-  self.setExecutionNode( eNode )
-  
+def initialization(self):
 
+    eNode = SerialExecutionNode(self.name, parameterized=self)
+
+    ###########################################################################
+    #           01 Create Freesurfer subject from T1 anatomical image         #
+    ###########################################################################
+
+    eNode.addChild(
+        "FreeSurfer01",
+        ProcessExecutionNode("freesurferCreateSubject", optional=1))
+
+    eNode.addDoubleLink("FreeSurfer01.RawT1Image", "RawT1Image")
+
+    eNode.addDoubleLink("FreeSurfer01.database", "database")
+
+    databases = [h.name for h in neuroHierarchy.hierarchies()
+                 if h.fso.name == "freesurfer"]
+    self.signature["database"].setChoices(*databases)
+    if len(databases) != 0:
+        self.database = databases[0]
+    else:
+        self.signature["database"] = OpenChoice()
+
+    ###########################################################################
+    #            02 Launch Freesurfer full pipeline recon-all                 #
+    ###########################################################################
+
+    eNode.addChild(
+        "FreeSurfer02",
+        ProcessExecutionNode("freesurferPipeline", optional=1))
+
+    eNode.addDoubleLink("FreeSurfer02.AnatImage", "FreeSurfer01.AnatImage")
+
+    ###########################################################################
+    #        FreeSurfer outputs To BrainVisa conversion pipeline              #
+    ###########################################################################
+
+    eNode.addChild(
+        "FreeSurferPipeline",
+        ProcessExecutionNode("freesurferToBrainvisaConversionPipeline",
+                             optional=1))
+
+    eNode.addDoubleLink("FreeSurferPipeline.anat", "FreeSurfer02.AnatImage")
+
+    eNode.FreeSurferPipeline.removeLink("nu", "anat")
+
+    eNode.FreeSurferPipeline.removeLink("ribbon", "anat")
+
+    eNode.FreeSurferPipeline.removeLink("leftPial", "anat")
+    eNode.FreeSurferPipeline.removeLink("leftCurv", "anat")
+
+    eNode.FreeSurferPipeline.removeLink("rightPial", "anat")
+    eNode.FreeSurferPipeline.removeLink("rightCurv", "anat")
+
+    eNode.addDoubleLink("FreeSurferPipeline.nu", "FreeSurfer02.nu")
+    eNode.addDoubleLink("FreeSurferPipeline.ribbon", "FreeSurfer02.ribbon")
+
+    # left hemisphere
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftPial", "FreeSurfer02.leftPial")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftWhite", "FreeSurfer02.leftWhite")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftSphereReg", "FreeSurfer02.leftSphereReg")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftThickness", "FreeSurfer02.leftThickness")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftCurv", "FreeSurfer02.leftCurv")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftAvgCurv", "FreeSurfer02.leftAvgCurv")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftCurvPial", "FreeSurfer02.leftCurvPial")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftGyri", "FreeSurfer02.leftGyri")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.leftSulciGyri", "FreeSurfer02.leftSulciGyri")
+
+    # right hemisphere
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightPial", "FreeSurfer02.rightPial")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightWhite", "FreeSurfer02.rightWhite")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightSphereReg", "FreeSurfer02.rightSphereReg")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightThickness", "FreeSurfer02.rightThickness")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightCurv", "FreeSurfer02.rightCurv")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightAvgCurv", "FreeSurfer02.rightAvgCurv")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightCurvPial", "FreeSurfer02.rightCurvPial")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightGyri", "FreeSurfer02.rightGyri")
+
+    eNode.addDoubleLink(
+        "FreeSurferPipeline.rightSulciGyri", "FreeSurfer02.rightSulciGyri")
+
+    self.setExecutionNode(eNode)
