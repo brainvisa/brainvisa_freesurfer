f171c98e2ee1ff6ffa483e6dec48be74f5a37f29
denisri <denis.riviere.gm@gmail.com>
2016-04-27 12:37:32 +0000

    fixes in types inheritance, and in viewers.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@72850 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Viewers/freesurferResampledGyriViewer.py
+++ brainvisa/toolboxes/freesurfer/processes/Viewers/freesurferResampledGyriViewer.py
@@ -11,16 +11,36 @@ def validation():
   anatomist.validation()
 
 signature = Signature(
-  'Gyri', ReadDiskItem('FreesurferResampledParcellationType',
-    'anatomist Texture formats'),
-  'WhiteMesh', ReadDiskItem('AimsWhite', 'anatomist mesh formats'),
+    'Gyri', ReadDiskItem('FreesurferResampledParcellationType',
+        'anatomist Texture formats'),
+    'WhiteMesh', ReadDiskItem('AimsWhite', 'anatomist mesh formats'),
+    "prefer_inflated_meshes", Boolean(),
 )
 
 def initialization( self ):
-  self.linkParameters('WhiteMesh', 'Gyri')
+    def link_mesh(self, dummy):
+        if self.Gyri is not None:
+            if self.prefer_inflated_meshes:
+                infl1 = 'Yes'
+                infl2 = 'No'
+            else:
+                infl1 = 'No'
+                infl2 = 'Yes'
+            mesh_type = self.signature["WhiteMesh"]
+            res = mesh_type.findValue(self.Gyri,
+                                      requiredAttributes={"inflated": infl1})
+            if res is None:
+                res = mesh_type.findValue(
+                    self.Gyri, requiredAttributes={"inflated": infl2})
+            if res is None:
+                res = self.Gyri
+            return res
+
+    self.linkParameters('WhiteMesh', ['Gyri', 'prefer_inflated_meshes'],
+                        link_mesh)
 
 
 def execution( self, context ):
-  a=anatomist.Anatomist()
-  return a.viewTextureOnMesh(self.WhiteMesh, self.Gyri, interpolation='rgb',
-                             palette='freesurfer_gyri')
+    a=anatomist.Anatomist()
+    return a.viewTextureOnMesh(self.WhiteMesh, self.Gyri, interpolation='rgb',
+                               palette='freesurfer_gyri')
