0903b92988c806f8b4c58f2bc783fa5f6d68d4f1
denisri <denis.riviere.gm@gmail.com>
2016-09-27 13:17:59 +0000

    keep and maintain labels tables in texture headers for label textures
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@74722 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/freesurfer/concatenate_textures.py
+++ python/freesurfer/concatenate_textures.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from numpy import array
 import numpy
 #from tio import Texture
@@ -5,34 +6,61 @@ from soma import aims
 import sys
 
 def usage():
-  print "concatenate textures"
-  #print "usage: python concatenate_textures.py output.tex file1.tex ... fileN.tex"
+    print("concatenate textures")
+    #print "usage: python concatenate_textures.py output.tex file1.tex ... fileN.tex"
 
-def concatenate_textures(output, fileL, fileR): 
-  gyriTexR = aims.read(fileR)
-  gyriTexL = aims.read(fileL)
-  gyriTexB = aims.TimeTexture_S16()
-  vertexNbR = gyriTexR[0].nItem()
-  vertexNbL = gyriTexL[0].nItem()
-  vertexNbB = vertexNbR + vertexNbL
-  gyriTexB[0].reserve(vertexNbB)
-  for v in xrange(vertexNbL):
-    gyriTexB[0].push_back(gyriTexL[0][v] + 1)
-    lh_max = gyriTexB[0].arraydata().max()
-  gyriTexB[0].arraydata()[gyriTexB[0].arraydata() == 0] = 1
-  for v in xrange(vertexNbR):
-    value = gyriTexR[0][v] + 1 + lh_max
-    if value == lh_max:
-      value = value + 1
-    gyriTexB[0].push_back(value)
-  aims.write(gyriTexB, output)
+def concatenate_textures(output, fileL, fileR):
+    print('*** concatenate_textures ***')
+    gyriTexR = aims.read(fileR)
+    gyriTexL = aims.read(fileL)
+    gyriTexB = gyriTexL.__class__()
+    vertexNbR = gyriTexR[0].nItem()
+    vertexNbL = gyriTexL[0].nItem()
+    vertexNbB = vertexNbR + vertexNbL
+    gyriTexB[0].resize(vertexNbB)
+    arr_b = gyriTexB[0].arraydata()
+    arr_b[:vertexNbL] = gyriTexL[0].arraydata() + 1
+    lh_max = gyriTexL[0].arraydata().max() + 2
+    arr_b[arr_b[:vertexNbL] == 0] = 1 # orig value -1 becomes 1
+    arr_b[vertexNbL:] = gyriTexR[0].arraydata() + lh_max
+    arr_b[vertexNbL:][gyriTexR[0].arraydata() == -1] = lh_max
+    # fix header
+    new_lt = {}
+    if 'GIFTI_labels_table' in gyriTexL.header():
+        print('copy GIFTI_labels_table')
+        lt = gyriTexL.header()['GIFTI_labels_table']
+        for i in range(lh_max - 1):
+            try:
+                new_lt[i + 1] = lt[i]
+            except:
+                pass
+    if 'GIFTI_labels_table' in gyriTexR.header():
+        lt = gyriTexR.header()['GIFTI_labels_table']
+        for i in range(gyriTexR[0].arraydata().max() + 1):
+            try:
+                new_lt[lh_max + i] = lt[i]
+            except:
+                pass
+    gyriTexB.header().update(gyriTexL.header())
+    print('new_lt:', new_lt)
+    if len(new_lt) != 0:
+        # aims bindings for IntKeyDictionary are incomplete, use the existing
+        # object
+        lt = gyriTexB.header()['GIFTI_labels_table']
+        for i, v in new_lt.items():
+            lt[i] = v
+        # should just be:
+        #gyriTexB.header()['GIFTI_labels_table'] = new_lt
+    print('header:')
+    print(gyriTexB.header())
+    aims.write(gyriTexB, output)
 
 if __name__ == "__main__":
-  if len(sys.argv)<=3:
-    usage()
-    sys.exit(1)
-  output = sys.argv[1]
-  fileL = sys.argv[2]
-  fileR = sus.argv[3]
-  concatenate_textures(output, fileL, fileR)
+    if len(sys.argv) <= 3:
+        usage()
+        sys.exit(1)
+    output = sys.argv[1]
+    fileL = sys.argv[2]
+    fileR = sus.argv[3]
+    concatenate_textures(output, fileL, fileR)
 
