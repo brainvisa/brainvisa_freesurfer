71d71bedf74ccb83947cba19cfdd6d50bcd8919b
denisri <denis.riviere.gm@gmail.com>
2016-10-05 15:38:46 +0000

    added possibility to save a time-sequence of inflated meshes
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@74789 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Tools/freesurferInflate.py
+++ brainvisa/toolboxes/freesurfer/processes/Tools/freesurferInflate.py
@@ -6,20 +6,35 @@ name = 'Meshes inflate'
 userlevel = 2
 
 signature = Signature(
-  'White', ReadDiskItem('AimsWhite', 'Aims mesh formats'),
-  'InflatedWhite', WriteDiskItem('AimsInflatedWhite', 'Aims mesh formats'),
-  'InflatedWhiteCurvTex', WriteDiskItem('AimsInflatedWhiteCurvTex', 'Aims Texture formats'),
-  )
+    'White', ReadDiskItem('AimsWhite', 'Aims mesh formats'),
+    'InflatedWhite', WriteDiskItem('AimsInflatedWhite', 'Aims mesh formats'),
+    'InflatedWhiteCurvTex', WriteDiskItem('AimsInflatedWhiteCurvTex',
+                                          'Aims Texture formats'),
+    'save_sequence', Boolean(),
+    'InflatedWhite_sequence',
+    WriteDiskItem('AimsInflatedWhite', 'Aims mesh formats',
+                  requiredAttributes={'time_sequence': 'Yes'}),
+)
+
 
 def initialization(self):
-  self.linkParameters('InflatedWhite', 'White')
-  self.linkParameters('InflatedWhiteCurvTex', 'White')
+    self.save_sequence = True
+    self.setOptional('InflatedWhite_sequence')
+    self.linkParameters('InflatedWhite', 'White')
+    self.linkParameters('InflatedWhiteCurvTex', 'White')
+    self.linkParameters('InflatedWhite_sequence', 'White')
 
-  
+
 def execution(self, context):
-  context.write(self.White.fullPath())
-  context.system( 'AimsInflate', '-i', self.White.fullPath(), '-o',
-                  self.InflatedWhite.fullPath(), '-c',
-                  self.InflatedWhiteCurvTex.fullPath())
-  tm = registration.getTransformationManager()
-  tm.copyReferential( self.White, self.InflatedWhite )
+    context.write(self.White.fullPath())
+    context.system('AimsInflate', '-i', self.White.fullPath(),
+                   '-o', self.InflatedWhite.fullPath(),
+                   '-c', self.InflatedWhiteCurvTex.fullPath())
+    tm = registration.getTransformationManager()
+    tm.copyReferential(self.White, self.InflatedWhite)
+    if self.save_sequence:
+        context.system('AimsInflate', '-i', self.White.fullPath(),
+                      '-o', self.InflatedWhite.fullPath(),
+                      '-c', self.InflatedWhiteCurvTex.fullPath(),
+                      '-S')
+        tm.copyReferential(self.White, self.InflatedWhite_sequence)
