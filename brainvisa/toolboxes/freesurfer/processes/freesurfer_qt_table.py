8d695d7bab3f9f03383cb62bf6779855c85a0a3b
denisri <denis.riviere.gm@gmail.com>
2017-10-15 22:54:42 +0000

    improved / completed Freesurfer QC table
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@79469 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/freesurfer_qt_table.py
+++ brainvisa/toolboxes/freesurfer/processes/freesurfer_qt_table.py
@@ -29,7 +29,51 @@ def initialization(self):
 def execution(self, context):
     dtypes = ['RawFreesurferAnat', 'Ribbon Freesurfer',
               'Freesurfer Scanner To MNI Transformation',
-              'Pial', 'Pial', 'White', 'White']
+              'Pial', 'Pial', 'White', 'White',
+              'AimsPial', 'AimsPial', 'AimsWhite', 'AimsWhite',
+              'FreesurferGyri', 'FreesurferGyri',
+              'FreesurferSulciGyri', 'FreesurferSulciGyri',
+              'ResampledGyri', 'ResampledGyri',
+              'ResampledSulciGyri', 'ResampledSulciGyri',
+              'ResampledFreesurferCurvType', 'ResampledFreesurferCurvType',
+              'ResampledFreesurferAvgCurvType',
+              'ResampledFreesurferAvgCurvType',
+              'ResampledFreesurferCurvPialType',
+              'ResampledFreesurferCurvPialType',
+              'ResampledFreesurferThicknessType',
+              'ResampledFreesurferThicknessType',
+              'AimsInflatedWhite', 'AimsInflatedWhite',
+              'AimsBothWhite', 'AimsBothPial', 'AimsBothInflatedWhite',
+              'BothResampledGyri', 'BothResampledSulciGyri']
+
+    tlabels = ['Raw T1', 'Ribbon',
+              'Freesurfer To MNI Transformation',
+              'Left Pial Mesh', 'Right Pial Mesh',
+              'Left White Mesh', 'Right White Mesh',
+              'Left Resampled Pial', 'Right Resampled Pial',
+              'Left Resampled White', 'Right Resampled White',
+              'Left Gyri Texture', 'Right Gyri Texture',
+              'Left Sulci/Gyri', 'Right Sulci/Gyri',
+              'Left Resampled Gyri', 'Right Resampled Gyri',
+              'Left Resampled Sulci/Gyri', 'Right Resampled Sulci/Gyri',
+              'Left Resampled Curvature', 'Right Resampled Curvature',
+              'Left Resampled Avg. Curvature',
+              'Right Resampled Avg. Curvature',
+              'Left Resampled Pial Curvature',
+              'Right Resampled Pial Curvature',
+              'Left Resampled Thickness',
+              'Right Resampled Thickness',
+              'Left Resampled Inflated White',
+              'Right Resampled Inflated White',
+              'Both Hemi. White', 'Both Hemi. Pial',
+              'Both Hemi. Inflated White',
+              'Both Hemi. Resampled Gyri', 'Both Hemi. Resampled Sulci/Gyri']
+
+    custom_filt = [eval(filt) for filt in self.data_filters]
+    if len(custom_filt) == 1:
+        custom_filt = custom_filt * len(dtypes)
+    if len(custom_filt) < len(dtypes):
+        custom_filt = custom_filt + [{}] * (len(dtypes) - len(custom_filt))
 
     filter1 = {'_format': ('NIFTI-1 image', 'gz compressed NIFTI-1 image')}
     filter2 = {}
@@ -37,13 +81,22 @@ def execution(self, context):
     filter3_r = {'side': 'right'}
 
     filters = [filter1, filter1, filter2, filter3_l, filter3_r,
-               filter3_l, filter3_r, ]
+               filter3_l, filter3_r, filter3_l, filter3_r,
+               filter3_l, filter3_r, filter3_l, filter3_r,
+               filter3_l, filter3_r, filter3_l, filter3_r,
+               filter3_l, filter3_r, filter3_l, filter3_r,
+               filter3_l, filter3_r, filter3_l, filter3_r,
+               filter3_l, filter3_r, filter3_l, filter3_r,
+               filter2, filter2, filter2, filter2, filter2]
+    for filt, custfilt in zip(filters, custom_filt):
+        filt.update(custfilt)
     filters = [repr(filt) for filt in filters]
 
-    context.write('filters:', filters)
     self.proc = getProcessInstance('database_qc_table')
     return context.runProcess(self.proc, database=self.database,
                               data_types=dtypes,
                               data_filters=filters,
-                              keys=self.keys)
+                              keys=self.keys,
+                              type_labels=tlabels)
+
 
