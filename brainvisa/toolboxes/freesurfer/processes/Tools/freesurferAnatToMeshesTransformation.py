244f4f305c79e855ea726a32ce79121028992beb
denisri <denis.riviere.gm@gmail.com>
2018-06-11 15:27:22 +0000

    autopep8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@81876 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Tools/freesurferAnatToMeshesTransformation.py
+++ brainvisa/toolboxes/freesurfer/processes/Tools/freesurferAnatToMeshesTransformation.py
@@ -6,35 +6,36 @@ from soma import aims
 name = 'Anat To FreeSurfer Meshes Transformation'
 
 signature = Signature(
-  'anat', ReadDiskItem( 'RawFreeSurferAnat', 'Aims readable volume formats' ),
+    'anat', ReadDiskItem('RawFreeSurferAnat', 'Aims readable volume formats'),
   'freesurfer_meshes_referential',
-    ReadDiskItem( 'Referential of Pial', 'Referential' ),
+    ReadDiskItem('Referential of Pial', 'Referential'),
   'anat_referential',
-    ReadDiskItem( 'Referential of Raw T1 MRI', 'Referential' ),
+    ReadDiskItem('Referential of Raw T1 MRI', 'Referential'),
   'anat_to_meshes_transform',
-    WriteDiskItem( 'Freesurfer Anat To Meshes Transformation',
-      'Transformation matrix' ),
+    WriteDiskItem('Freesurfer Anat To Meshes Transformation',
+                  'Transformation matrix'),
 )
 
 
-def initialization( self ):
-  self.linkParameters( 'anat_referential', 'anat' )
-  self.linkParameters( 'freesurfer_meshes_referential', 'anat' )
-  self.linkParameters( 'anat_to_meshes_transform',
-    'freesurfer_meshes_referential' )
+def initialization(self):
+    self.linkParameters('anat_referential', 'anat')
+    self.linkParameters('freesurfer_meshes_referential', 'anat')
+    self.linkParameters('anat_to_meshes_transform',
+                        'freesurfer_meshes_referential')
 
 
-def execution( self, context ):
-  atts = aimsGlobals.aimsVolumeAttributes( self.anat )
-  tr = aims.AffineTransformation3d( atts[ 'transformations' ][-1] )
-  aims.write( tr, self.anat_to_meshes_transform.fullPath() )
-  self.anat_to_meshes_transform.setMinf( 'source_referential',
-    self.anat_referential.uuid(), saveMinf=False )
-  self.anat_to_meshes_transform.setMinf( 'destination_referential',
-    self.freesurfer_meshes_referential.uuid(), saveMinf=True )
-  tm = registration.getTransformationManager()
-  tm.setNewTransformationInfo( self.anat_to_meshes_transform,
-    source_referential=self.anat_referential,
-    destination_referential=self.freesurfer_meshes_referential )
-
-
+def execution(self, context):
+    atts = aimsGlobals.aimsVolumeAttributes(self.anat)
+    tr = aims.AffineTransformation3d(atts['transformations'][-1])
+    aims.write(tr, self.anat_to_meshes_transform.fullPath())
+    self.anat_to_meshes_transform.setMinf('source_referential',
+                                          self.anat_referential.uuid(),
+                                          saveMinf=False)
+    self.anat_to_meshes_transform.setMinf(
+        'destination_referential', self.freesurfer_meshes_referential.uuid(),
+        saveMinf=True)
+    tm = registration.getTransformationManager()
+    tm.setNewTransformationInfo(
+        self.anat_to_meshes_transform,
+        source_referential=self.anat_referential,
+        destination_referential=self.freesurfer_meshes_referential)
