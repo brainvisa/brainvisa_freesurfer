8ecae6f8c25fa528c79d8f621437e8e07ab402a6
denisri <denis.riviere.gm@gmail.com>
2018-06-11 14:20:21 +0000

    autopep8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@81870 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Tools/freesurferAnatToNii.py
+++ brainvisa/toolboxes/freesurfer/processes/Tools/freesurferAnatToNii.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import os, sys
+import os
+import sys
 from brainvisa.processes import *
 from freesurfer.brainvisaFreesurfer import launchFreesurferCommand
 from brainvisa import registration
@@ -8,93 +9,100 @@ name = "Convert Freesurfer images to Nifti format"
 userLevel = 2
 
 signature = Signature(
-  'AnatImage', ReadDiskItem(
-    'RawFreesurferAnat',
-    'FreesurferMGZ',
-    enableConversion=False),
+    'AnatImage', ReadDiskItem(
+        'RawFreesurferAnat',
+        'FreesurferMGZ',
+        enableConversion=False),
   'NiiAnatImage', WriteDiskItem(
-    'RawFreesurferAnat',
-    ['gz compressed NIFTI-1 image', 'NIFTI-1 image']),
+      'RawFreesurferAnat',
+      ['gz compressed NIFTI-1 image', 'NIFTI-1 image']),
   'NuImage', ReadDiskItem(
-    'Nu FreesurferAnat',
-    'FreesurferMGZ',
-    enableConversion=False),
+      'Nu FreesurferAnat',
+      'FreesurferMGZ',
+      enableConversion=False),
   'NiiNuImage', WriteDiskItem(
-    'Nu FreesurferAnat',
-    ['gz compressed NIFTI-1 image', 'NIFTI-1 image']),
+      'Nu FreesurferAnat',
+      ['gz compressed NIFTI-1 image', 'NIFTI-1 image']),
   'RibbonImage', ReadDiskItem(
-    'Ribbon Freesurfer',
-    'FreesurferMGZ',
-    enableConversion=False),
+      'Ribbon Freesurfer',
+      'FreesurferMGZ',
+      enableConversion=False),
   'NiiRibbonImage', WriteDiskItem(
-    'Ribbon Freesurfer',
-    ['gz compressed NIFTI-1 image', 'NIFTI-1 image']),
+      'Ribbon Freesurfer',
+      ['gz compressed NIFTI-1 image', 'NIFTI-1 image']),
   'referential', ReadDiskItem(
-    'Referential of Raw T1 MRI',
-    'Referential'),
+      'Referential of Raw T1 MRI',
+      'Referential'),
 )
 
+
 def initialization(self):
-  self.linkParameters( 'NiiAnatImage', 'AnatImage' )
-  self.linkParameters( 'NuImage', 'AnatImage' )
-  self.linkParameters( 'NiiNuImage', 'NuImage' )
-  self.linkParameters( 'RibbonImage', 'AnatImage' )
-  self.linkParameters( 'NiiRibbonImage', 'RibbonImage' )
-  self.linkParameters( 'referential', 'AnatImage' )
-  self.setOptional( 'referential' )
+    self.linkParameters('NiiAnatImage', 'AnatImage')
+    self.linkParameters('NuImage', 'AnatImage')
+    self.linkParameters('NiiNuImage', 'NuImage')
+    self.linkParameters('RibbonImage', 'AnatImage')
+    self.linkParameters('NiiRibbonImage', 'RibbonImage')
+    self.linkParameters('referential', 'AnatImage')
+    self.setOptional('referential')
+
 
 def execution(self, context):
-  
-  if os.path.exists( self.NiiAnatImage.minfFileName() ):
-    context.write( 'removing residual .minf file %s' % self.NiiAnatImage.minfFileName() )
-    os.unlink( self.NiiAnatImage.minfFileName() )
-  if os.path.exists( self.NiiNuImage.minfFileName() ):
-    context.write( 'removing residual .minf file %s' % self.NiiNuImage.minfFileName() )
-    os.unlink( self.NiiNuImage.minfFileName() )
-  if os.path.exists( self.NiiRibbonImage.minfFileName() ):
-    context.write( 'removing residual .minf file %s' % self.NiiRibbonImage.minfFileName() )
-    os.unlink( self.NiiRibbonImage.minfFileName() )
-  
-  # convert from .mgz to .nii with Freesurfer
-  launchFreesurferCommand( context, '',
-                           'mri_convert',
-                           self.AnatImage.fullPath(),
-                           self.NiiAnatImage.fullPath() )
-  launchFreesurferCommand( context, '',
-                           'mri_convert',
-                           self.NuImage.fullPath(),
-                           self.NiiNuImage.fullPath() )                        
-  launchFreesurferCommand( context, '',
-                           'mri_convert',
-                           self.RibbonImage.fullPath(),
-                           self.NiiRibbonImage.fullPath() )
-                           
-  # reset minf attributes in case there was an existing older diskitem
-  self.NiiAnatImage._minfAttributes = {}
-  self.NiiNuImage._minfAttributes = {}
-  self.NiiRibbonImage._minfAttributes = {}
-  self.NiiAnatImage.saveMinf()
-  self.NiiNuImage.saveMinf()
-  self.NiiRibbonImage.saveMinf()
-  context.system(os.path.basename(sys.executable), '-c', 'from freesurfer.setAnatTransformation import setAnatTransformation as f; f(\"%s\");'%(self.NiiAnatImage.fullPath()))
-  context.system(os.path.basename(sys.executable), '-c', 'from freesurfer.setAnatTransformation import setAnatTransformation as f; f(\"%s\");'%(self.NiiNuImage.fullPath()))
-  context.system(os.path.basename(sys.executable), '-c', 'from freesurfer.setAnatTransformation import setAnatTransformation as f; f(\"%s\");'%(self.NiiRibbonImage.fullPath()))
-  self.NiiAnatImage.readAndUpdateMinf()
-  self.NiiNuImage.readAndUpdateMinf()
-  self.NiiRibbonImage.readAndUpdateMinf()
-  
-  # referential
-  if self.referential is not None:
-    self.NiiAnatImage.setMinf( 'referential', self.referential.uuid(),
-      saveMinf=True )
-    self.NiiNuImage.setMinf( 'referential', self.referential.uuid(),
-      saveMinf=True ) 
-    self.NiiRibbonImage.setMinf( 'referential', self.referential.uuid(),
-      saveMinf=True )
-  registration.getTransformationManager().copyReferential( self.referential,
-    self.NiiAnatImage )
-  registration.getTransformationManager().copyReferential( self.referential,
-    self.NiiNuImage )  
-  registration.getTransformationManager().copyReferential( self.referential,
-    self.NiiRibbonImage ) 
 
+    if os.path.exists(self.NiiAnatImage.minfFileName()):
+        context.write('removing residual .minf file %s' %
+                      self.NiiAnatImage.minfFileName())
+        os.unlink(self.NiiAnatImage.minfFileName())
+    if os.path.exists(self.NiiNuImage.minfFileName()):
+        context.write('removing residual .minf file %s' %
+                      self.NiiNuImage.minfFileName())
+        os.unlink(self.NiiNuImage.minfFileName())
+    if os.path.exists(self.NiiRibbonImage.minfFileName()):
+        context.write('removing residual .minf file %s' %
+                      self.NiiRibbonImage.minfFileName())
+        os.unlink(self.NiiRibbonImage.minfFileName())
+
+    # convert from .mgz to .nii with Freesurfer
+    launchFreesurferCommand(context, '',
+                            'mri_convert',
+                            self.AnatImage.fullPath(),
+                            self.NiiAnatImage.fullPath())
+    launchFreesurferCommand(context, '',
+                            'mri_convert',
+                            self.NuImage.fullPath(),
+                            self.NiiNuImage.fullPath())
+    launchFreesurferCommand(context, '',
+                            'mri_convert',
+                            self.RibbonImage.fullPath(),
+                            self.NiiRibbonImage.fullPath())
+
+    # reset minf attributes in case there was an existing older diskitem
+    self.NiiAnatImage._minfAttributes = {}
+    self.NiiNuImage._minfAttributes = {}
+    self.NiiRibbonImage._minfAttributes = {}
+    self.NiiAnatImage.saveMinf()
+    self.NiiNuImage.saveMinf()
+    self.NiiRibbonImage.saveMinf()
+    context.system(os.path.basename(sys.executable), '-c',
+                   'from freesurfer.setAnatTransformation import setAnatTransformation as f; f(\"%s\");' % (self.NiiAnatImage.fullPath()))
+    context.system(os.path.basename(sys.executable), '-c',
+                   'from freesurfer.setAnatTransformation import setAnatTransformation as f; f(\"%s\");' % (self.NiiNuImage.fullPath()))
+    context.system(os.path.basename(sys.executable), '-c',
+                   'from freesurfer.setAnatTransformation import setAnatTransformation as f; f(\"%s\");' % (self.NiiRibbonImage.fullPath()))
+    self.NiiAnatImage.readAndUpdateMinf()
+    self.NiiNuImage.readAndUpdateMinf()
+    self.NiiRibbonImage.readAndUpdateMinf()
+
+    # referential
+    if self.referential is not None:
+        self.NiiAnatImage.setMinf('referential', self.referential.uuid(),
+                                  saveMinf=True)
+        self.NiiNuImage.setMinf('referential', self.referential.uuid(),
+                                saveMinf=True)
+        self.NiiRibbonImage.setMinf('referential', self.referential.uuid(),
+                                    saveMinf=True)
+    registration.getTransformationManager().copyReferential(self.referential,
+                                                            self.NiiAnatImage)
+    registration.getTransformationManager().copyReferential(self.referential,
+                                                            self.NiiNuImage)
+    registration.getTransformationManager().copyReferential(self.referential,
+                                                            self.NiiRibbonImage)
