a73d509bbe98989045fcdf799196f7d201aac373
denisri <denis.riviere.gm@gmail.com>
2018-06-14 16:14:28 +0000

    fixes for python3
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/branches/bug_fix@81971 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/freesurfer/regularizeTexture.py
+++ python/freesurfer/regularizeTexture.py
@@ -1,7 +1,9 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python
+from __future__ import print_function
+
 from numpy import vstack, array, zeros
 import pickle
-from tio import Texture
+from .tio import Texture
 import sys
 from soma import aims
 
@@ -25,20 +27,20 @@ def regularizeTexture(isin, mesh, tex, output):
 
 
 def usage():
-    print "Regularize texture"
-    print "usage : regularizeTexture.py isin original_white_mesh input.tex output.tex"
-    print "example : regularizeTexture.py lh.isin lh.white.mesh lh.curv.tex lh.r.curv.tex"
+    print("Regularize texture")
+    print("usage : regularizeTexture.py isin original_white_mesh input.tex output.tex")
+    print("example : regularizeTexture.py lh.isin lh.white.mesh lh.curv.tex lh.r.curv.tex")
 
 
 if __name__ == "__main__":
     if len(sys.argv)!=5:
         usage()
         sys.exit(1)
-    print sys.argv
-    print "Isin file:", sys.argv[1]
-    print "White mesh:", sys.argv[2]
-    print "Input texture:", sys.argv[3]
-    print "Output texture:", sys.argv[4]
+    print(sys.argv)
+    print("Isin file:", sys.argv[1])
+    print("White mesh:", sys.argv[2])
+    print("Input texture:", sys.argv[3])
+    print("Output texture:", sys.argv[4])
     regularizeTexture(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
 
