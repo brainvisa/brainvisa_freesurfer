192c8b2dde39f7f745cde23eef37364f37a65007
nsouedet <nicolas.souedet@gmail.com>
2020-04-01 21:03:11 +0000

    fixes: absolute import
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/freesurfer_plugin/trunk@86307 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/freesurfer/processes/Tools/freesurferAnatToNii.py
+++ brainvisa/toolboxes/freesurfer/processes/Tools/freesurferAnatToNii.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import absolute_import
 from __future__ import print_function
 import os
 import sys
 
-from __future__ import absolute_import
 from brainvisa.processes import *
 from freesurfer.brainvisaFreesurfer import launchFreesurferCommand
 from brainvisa import registration
